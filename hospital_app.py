5f9d9fc8c0120edf04b1abc6f685e806baac31cd
KodingNextFC <yuan@kodingnext.com>
2026-06-03 09:17:50 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -12,4 +12,19 @@ st.header("Symptoms")
 fever = st.checkbox("Fever")
 cough = st.checkbox("Cough")
 headache = st.checkbox("Headache")
+chest_pain = st.checkbox("Chest Pain")
+
+#Patient Information
+st.header("Patient Information")
+
+age = st.number_input(
+    "Age",
+    min_value=1,
+    max_value=120,
+    value=35
+)
+gender = st.selectbox(
+    "Gender",
+    ["Female", "Male"]
+)
 
