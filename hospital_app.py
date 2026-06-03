6fd3f3aee2dd6d9cdf6da817a98d9a39370fc070
KodingNextFC <yuan@kodingnext.com>
2026-06-03 09:16:38 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -5,3 +5,11 @@ st.set_page_config(
 )
 st.title("🏥 Smart Hospital Patient Navigator")
 st.write("Find the Right Department for Your Symptomps")
+
+#symptoms
+st.header("Symptoms")
+
+fever = st.checkbox("Fever")
+cough = st.checkbox("Cough")
+headache = st.checkbox("Headache")
+
